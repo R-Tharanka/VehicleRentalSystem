f1a1892162c0979558007b36fd5bca1383e6493c
Ruchira Tharanka <150226449+R-Tharanka@users.noreply.github.com>
2025-04-16 14:44:47 +0530

    Make Motorbike class public and add sidecar feature
    Updated the `Motorbike` class to be public, added a `HasSideCar` property, and implemented a constructor to initialize the object. Overrode the `GetInfo` method to include sidecar information in the output.

--- Models/Motorbike.cs
+++ Models/Motorbike.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace VehicleRentalSystem.Models
 {
-    internal class Motorbike
+    public class Motorbike : Vehicle
     {
+        public bool HasSideCar { get; set; }
+
+        public Motorbike(string regNumber, string brand, int year, bool hasSideCar)
+            : base(regNumber, brand, year)
+        {
+            HasSideCar = hasSideCar;
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + $" | Sidecar: {(HasSideCar ? "Yes" : "No")}";
+        }
     }
 }
+
